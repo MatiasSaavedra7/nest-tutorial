303dd4f1e908c11bb775fc59421c02d305319e95
Matias Saavedra <matiassaavedra1995@gmail.com>
2026-03-18 17:09:20 -0300

    Agrego autenticacion requerida para las funciones de Crear, Actualizar y Eliminar registros

--- src/books/books.controller.ts
+++ src/books/books.controller.ts
@@ -8,13 +8,16 @@ import {
   Body,
   Query,
   Req,
+  UseGuards,
 } from '@nestjs/common';
 import { BooksService } from './books.service';
 import type { Request } from 'express';
 import { BookDto } from './book.dto';
 import { Book } from './book.entity';
+import { AuthGuard } from '@nestjs/passport';
 
 @Controller('books')
+// @UseGuards(AuthGuard('jwt'))
 export class BooksController {
   constructor(private booksService: BooksService) {}
 
@@ -24,21 +27,26 @@ export class BooksController {
   }
 
   @Get(':id')
+  @UseGuards(AuthGuard('jwt'))
   async findOne(@Param('id') id: string): Promise<Book | string> {
     return await this.booksService.findOne(id);
   }
 
   @Post()
+  @UseGuards(AuthGuard('jwt'))
   async create(@Body() newBook: BookDto): Promise<Book> {
     return await this.booksService.create(newBook);
   }
 
+
   @Put(':id')
+  @UseGuards(AuthGuard('jwt'))
   async update(@Param('id') id: string, @Body() updatedBook: BookDto): Promise<Book> {
     return await this.booksService.update(id, updatedBook);
   }
 
   @Delete(':id')
+  @UseGuards(AuthGuard('jwt'))
   async delete(@Param('id') id: string): Promise<Book> {
     return await this.booksService.delete(id);
   }
--- src/books/books.module.ts
+++ src/books/books.module.ts
@@ -1,13 +1,13 @@
-import { Module } from "@nestjs/common";
-import { Book } from "./book.entity";
-import { BooksService } from "./books.service";
-import { BooksController } from "./books.controller";
-import { TypeOrmModule } from "@nestjs/typeorm";
+import { Module } from '@nestjs/common';
+import { Book } from './book.entity';
+import { BooksService } from './books.service';
+import { BooksController } from './books.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { AuthModule } from 'src/utilities/auth.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Book])],
+  imports: [TypeOrmModule.forFeature([Book]), AuthModule],
   providers: [BooksService],
-  controllers: [BooksController]
+  controllers: [BooksController],
 })
-
 export class BooksModule {}
